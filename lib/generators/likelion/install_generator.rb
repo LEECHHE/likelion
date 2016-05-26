514d113ba754ad82035cc1c574a7be2db675f363
ChangHee <leechhe90@gmail.com>
2016-05-26 17:47:52 +0900

    debug generator

--- lib/generators/likelion/install_generator.rb
+++ lib/generators/likelion/install_generator.rb
@@ -1,8 +1,10 @@
 module Likelion
 	class InstallGenerator < Rails::Generators::NamedBase
 	  	desc "This generator creates an initializer file at config/initializers"
+
   		def copy_snippet
-  			run("ruby ./templates/create_templates.rb #{file_name}")
+  			path = File.dirname(__FILE__)	
+  			run("ruby #{path}/create_templates.rb #{file_name}")
   		end
 	end
 end
