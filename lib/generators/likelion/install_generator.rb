fb28a996088bdd6514f1d6fee38e69b5537d5c32
ChangHee <leechhe90@gmail.com>
2016-05-27 15:51:19 +0900

    add usage

--- lib/generators/likelion/install_generator.rb
+++ lib/generators/likelion/install_generator.rb
@@ -1,7 +1,7 @@
 module Likelion
 	class InstallGenerator < Rails::Generators::NamedBase
 		@@path = File.dirname(__FILE__)	
-	  	desc: "#{@@path}/USAGE"
+	  	desc "#{@@path}/USAGE"
   		def copy_snippet
   			run("ruby #{@@path}/create_templates.rb #{file_name}")
   		end
