7d1347dd8458afcecb0dd79a47c9f1d79c1760e1
ChangHee <leechhe90@gmail.com>
2016-05-27 16:21:03 +0900

    update readme for 160411

--- lib/generators/likelion/templates/160411/home_controller.rb
+++ lib/generators/likelion/templates/160411/home_controller.rb
@@ -1,9 +1,24 @@
+require 'mailgun'
+
 class HomeController < ApplicationController
-	def index
-	end
-	def write
-		@title = params[:title]
-		@receiver = params[:email]
-		@content = params[:content]
-	end
+  def index
+  end
+  def write
+    @title = params[:title]
+    @receiver = params[:email]
+    @content = params[:content]
+    #mail_sending
+    mg_client = Mailgun::Client.new("Api key")
+    message_params =  {
+                       from: 'master@example.com',
+                       to:   @receiver,
+                       subject: @title,
+                       text:    @content
+                      }
+    
+    result = mg_client.send_message('샌드박스', message_params).to_h!
+    
+    message_id = result['id']
+    message = result['message']
+  end
 end
